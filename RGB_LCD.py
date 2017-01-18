4d4bbca88b21083cff60f975d5eb5bd973a53c3d
Nieki <1452657houtvast@zuyd.nl>
2017-01-18 15:00:42 +0100

    'Fouten' eruit gehaald

--- RGB_LCD.py
+++ RGB_LCD.py
@@ -40,16 +40,16 @@ THE SOFTWARE.
 
 import time,sys
 
-if sys.platform == 'uwp'
+if sys.platform == 'uwp':
     import winrt_smbus as smbus
     bus = smbus.SMBus(1)
-else
+else:
     import smbus
     import RPi.GPIO as GPIO
     rev = GPIO.RPI_REVISION
-    if rev == 2 or rev == 3
+    if rev == 2 or rev == 3:
         bus = smbus.SMBus(1)
-    else
+    else:
         bus = smbus.SMBus(0)
 
 # this device has two I2C addresses
@@ -57,67 +57,67 @@ DISPLAY_RGB_ADDR = 0x62
 DISPLAY_TEXT_ADDR = 0x3e
 
 # set backlight to (R,G,B) (values from 0..255 for each)
-def setRGB(r,g,b)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,0,0)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,1,0)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,0x08,0xaa)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,4,r)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,3,g)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,2,b)
+def setRGB(r,g,b):
+    bus.write_byte_data(DISPLAY_RGB_ADDR, 0, 0)
+    bus.write_byte_data(DISPLAY_RGB_ADDR, 1, 0)
+    bus.write_byte_data(DISPLAY_RGB_ADDR, 0x08, 0xaa)
+    bus.write_byte_data(DISPLAY_RGB_ADDR, 4, r)
+    bus.write_byte_data(DISPLAY_RGB_ADDR, 3, g)
+    bus.write_byte_data(DISPLAY_RGB_ADDR, 2, b)
 
 # send command to display (no need for external use)    
-def textCommand(cmd)
-    bus.write_byte_data(DISPLAY_TEXT_ADDR,0x80,cmd)
+def textCommand(cmd):
+    bus.write_byte_data(DISPLAY_TEXT_ADDR, 0x80, cmd)
 
 # set display text n for second line(or auto wrap)     
-def setText(text)
-    textCommand(0x01) # clear display
+def setText(text):
+    textCommand(0x01)  # clear display
     time.sleep(.05)
-    textCommand(0x08  0x04) # display on, no cursor
-    textCommand(0x28) # 2 lines
+    textCommand(0x08, 0x04)  # display on, no cursor
+    textCommand(0x28)  # 2 lines
     time.sleep(.05)
     count = 0
     row = 0
-    for c in text
-        if c == 'n' or count == 16
+    for c in text:
+        if c == 'n' or count == 16:
             count = 0
             row += 1
-            if row == 2
+            if row == 2:
                 break
             textCommand(0xc0)
-            if c == 'n'
+            if c == 'n':
                 continue
         count += 1
         bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
 
 #Update the display without erasing the display
-def setText_norefresh(text)
+def setText_norefresh(text):
     textCommand(0x02) # return home
     time.sleep(.05)
 	
-    textCommand(0x08  0x04) # display on, no cursor
+    textCommand(0x08,0x04) # display on, no cursor
     textCommand(0x28) # 2 lines
     time.sleep(.05)
     count = 0
     row = 0
-    for c in text
-        if c == 'n' or count == 16
+    for c in text:
+        if c == 'n' or count == 16:
             count = 0
             row += 1
-            if row == 2
+            if row == 2:
                 break
             textCommand(0xc0)
-            if c == 'n'
+            if c == 'n':
                 continue
         count += 1
-        bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
+        bus.write_byte_data(DISPLAY_TEXT_ADDR, 0x40, ord(c))
 		
 # example code
-if __name__==__main__
-    setText(Hello worldnThis is an LCD test)
-    setRGB(0,128,64)
-    for c in range(0,255)
-        setRGB(c,255-c,0)
+if __name__==__main__:
+    setText("Hello worldnThis is an LCD test")
+    setRGB(0, 128, 64)
+    for c in range(0,255):
+        setRGB(c, 255-c, 0)
         time.sleep(0.01)
-    setRGB(0,255,0)
-    setText(Bye bye, this should wrap onto next line)
+    setRGB(0, 255, 0)
+    setText("Bye bye, this should wrap onto next line")
