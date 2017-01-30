ebc73378db8d25d3ee66d02062f1f91d8b4c223a
Nieki <1452657houtvast@zuyd.nl>
2017-01-30 11:26:36 +0100

    Push

--- Device_Code.py
+++ Device_Code.py
@@ -103,10 +103,10 @@ def Dispense():
 
     Set_State(STATE_DISPENSING)
 
-    nextDispense                    = -1
-    rgbColor                        = COLOR_BLUE
-    ledColor                        = COLOR_BLUE
-    buzzerTone                      = TONE_DISPENSING
+    nextDispense = -1
+    rgbColor = COLOR_BLUE
+    ledColor = COLOR_BLUE
+    buzzerTone = TONE_DISPENSING
 
     Set_Actuators()
     Set_Display("  Uw medicatie  ", "wordt uitgegeven")
@@ -115,10 +115,10 @@ def Dispense():
 
     Set_State(STATE_ALARMING)
 
-    dispenseTime                    = int(time())
-    rgbColor                        = COLOR_WHITE
-    ledColor                        = COLOR_ORANGE
-    buzzerTone                      = TONE_ALARMING
+    dispenseTime = int(time())
+    rgbColor = COLOR_WHITE
+    ledColor = COLOR_ORANGE
+    buzzerTone = TONE_ALARMING
 
     Set_Actuators()
     Set_Display("  Uw medicatie  ", "  ligt gereed   ")
@@ -132,16 +132,18 @@ def Inactive():
 def Active():
     global remainingTime
 
-    if nextDispense == -1:
-        Set_Next_Dispense()
+    # if nextDispense == -1:
+    #    Set_Next_Dispense()
 
-    print("NextDispense: ", nextDispense)
+    # print("NextDispense: ", nextDispense)
     print("Time Now: ", (datetime.now().second + datetime.now().minute * 60 + datetime.now().hour * 60))
-    if userInput == INPUT_TYPE_LONG or nextDispense <= (datetime.now().second + datetime.now().minute * 60 + datetime.now().hour * 60):
+    if userInput == INPUT_TYPE_LONG:  # or nextDispense <= (datetime.now().second + datetime.now().minute * 60 + datetime.now().hour * 60):
         Dispense()
+    '''
     elif (remainingTime != Get_Remaining()):
             remainingTime = Get_Remaining()
             Set_Display("Volgende inname:", Get_Remaining())
+'''
 
 
 def Dispensed():
@@ -149,15 +151,15 @@ def Dispensed():
     # Read ultra sonic sensor value
 
     # if EMPTY_HOLDER_VALUE + ERROR_MARGIN > readValue > EMPTY_HOLDER_VALUE - ERROR_MARGIN:
-        # Conclude blister taken by user
-        # systemState = STATE_ACTIVE
+    # Conclude blister taken by user
+    # systemState = STATE_ACTIVE
 
     # if time() > TIME_ALARM_USER:
-        # New alarm to alert user to take blister
+    # New alarm to alert user to take blister
 
     # if time() > TIME_ALERT_RESPONDERS:
-        # Alert connected responders about user not taking blister
-        # systemState = STATE_NOTIFYING
+    # Alert connected responders about user not taking blister
+    # systemState = STATE_NOTIFYING
 
 
 def Alarm():
@@ -174,9 +176,9 @@ def Notifying():
 
 # Geeft de tijd aan waarop de volgende inname plaatsvindt.
 def Get_Remaining():
-    minutes, seconds = divmod(nextDispense - 120, 60) #Waarom -2 minuten???
-    hours,   minutes = divmod(minutes, 60)
-    return("    " + "%02d:%02d" % (hours, minutes) + "    ")
+    minutes, seconds = divmod(nextDispense - 120, 60)  # Waarom -2 minuten???
+    hours, minutes = divmod(minutes, 60)
+    return ("    " + "%02d:%02d" % (hours, minutes) + "    ")
 
 
 # Verandert de state en schrijft dit in het logfile
@@ -192,6 +194,7 @@ def Set_Display(textTop, textBottom):
     setText(textTop[:MAX_LINE_CHARS] + "\n" + textBottom[:MAX_LINE_CHARS])
     print(textTop[:MAX_LINE_CHARS] + "\n" + textBottom[:MAX_LINE_CHARS])
 
+
 # ?
 def Set_Actuators():
     setRGB(rgbColor[0], rgbColor[1], rgbColor[2])
@@ -202,6 +205,7 @@ def Set_Actuators():
 # H
 def Set_Hardware():
     grovepi.pinMode(BUTTON_PIN, "INPUT")
+
 
 #
 def Set_Next_Dispense():
@@ -241,6 +245,7 @@ def Get_Timestamps():
         else:
             print("Error: Tijd", time, "voldoet niet aan eisen (HH:MM)")
 
+
 def Check_Active():
     global systemState
     global ledColor
@@ -248,9 +253,9 @@ def Check_Active():
 
     # Check and process power button input
     if userInput == INPUT_TYPE_POWER:
-        systemState     = STATE_ACTIVE  if systemState == STATE_INACTIVE else STATE_INACTIVE
-        ledColor        = COLOR_RED     if systemState == STATE_INACTIVE else COLOR_GREEN
-        rgbColor        = COLOR_DIMMED  if systemState == STATE_INACTIVE else COLOR_WHITE
+        systemState = STATE_ACTIVE if systemState == STATE_INACTIVE else STATE_INACTIVE
+        ledColor = COLOR_RED if systemState == STATE_INACTIVE else COLOR_GREEN
+        rgbColor = COLOR_DIMMED if systemState == STATE_INACTIVE else COLOR_WHITE
 
 
 def Check_Input():
@@ -266,22 +271,22 @@ def Check_Input():
 
     print(time())
 
-    if grovepi.digitalRead(BUTTON_PIN) == 1:                    # Button 1 wordt ingedrukt
+    if grovepi.digitalRead(BUTTON_PIN) == 1:  # Button 1 wordt ingedrukt
         if not buttonDown:
             buttonDown = True
             inputStart = int(time() * 1000)
     else:
-        if buttonDown:                                        # Button 1 wordt losgelaten
+        if buttonDown:  # Button 1 wordt losgelaten
             buttonDown = False
             inputRelease = int(time() * 1000)
             inputDuration = (inputRelease - inputStart)
             if inputDuration <= inputInterval:
-                userInput = INPUT_TYPE_SHORT          # Short press
+                userInput = INPUT_TYPE_SHORT  # Short press
             elif inputDuration < powerInterval:
-                userInput = INPUT_TYPE_LONG           # Long press
+                userInput = INPUT_TYPE_LONG  # Long press
             elif inputDuration >= powerInterval:
                 Set_Display("  Shutting down ", "    Goodbye     ")
-                userInput = INPUT_TYPE_POWER          # Shutdown press
+                userInput = INPUT_TYPE_POWER  # Shutdown press
 
 
 def Play_Intro():
@@ -296,6 +301,7 @@ def logWrite(logLine):
     logFile = open("action.log", "a")
     logFile.write(logLine + "\n")
     logFile.close()
+
 
 def Get_Dispense_Times():
     global dispenseTimeStamps
@@ -330,6 +336,7 @@ def Get_Dispense_Times():
     for earlyTime in tempTimes:
         dispenseTimeStamps.append(earlyTime)
 
+
 def timeForTakeOut():
     global currentTime
     global nextDispense
@@ -341,9 +348,9 @@ def timeForTakeOut():
 
     if currentTime == nextDispense and not dispensed:
         print("Je pillen liggen klaar!")
-                # dispense de blisters
+        # dispense de blisters
         dispensed = True
-        Set_State(STATE_ALARMING)
+        Set_State(STATE_DISPENSED)
 
     elif currentTime != nextDispense and not dispensed:
         print("Blijf van die pillen af")
@@ -351,6 +358,7 @@ def timeForTakeOut():
 
     elif dispensed:
         print('Je pillen liggen klaar!')
+        Set_State(STATE_ALARMING)
         # niet vergeten dispensed naar False te zetten als weggepakt/opgelost
 
 
